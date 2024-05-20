2170df88ab98080e00cc94f01265818c1e5ee86d
syscore24 <himanshu.bhalala@sys-core.com>
2024-05-20 12:08:42 +0530

    Cart access to users only

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,11 +35,13 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                @can('role', 'user')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex me-4">
                     <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
                         <i class="fa-solid fa-cart-shopping"></i>
                     </x-nav-link>
                 </div>
+                @endcan
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
